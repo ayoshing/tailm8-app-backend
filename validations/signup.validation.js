2153e71b70dfd1c810ef9e30c5a0a1502f4144e5
Shing Wong <shing2487@gmail.com>
2018-09-09 16:39:23 -0400

    added validation for signup input

---
+++ validations/signup.validation.js
@@ -0,0 +1,48 @@
+const validator = require("validator");
+const isEmpty = require("./isEmpty");
+
+module.exports = function validateSignUpInput(input) {
+  let errors = {};
+
+  input.name = !isEmpty(input.name) ? input.name : "";
+  input.email = !isEmpty(input.email) ? input.email : "";
+  input.password = !isEmpty(input.password) ? input.password : "";
+  input.password2 = !isEmpty(input.password2) ? input.password2 : "";
+
+  if (!validator.isLength(input.name, { min: 2, max: 30 })) {
+    errors.name = "Name must be between 2 and 30 charactors";
+  }
+
+  if (validator.isEmpty(input.name)) {
+    errors.name = "Name field is required";
+  }
+
+  if (validator.isEmpty(input.email)) {
+    errors.email = "Email field is required";
+  }
+
+  if (validator.isEmail(input.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (validator.isEmpty(input.password)) {
+    errors.password = "Password field is required";
+  }
+
+  if (!validator.isLength(input.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 charactors";
+  }
+
+  if (validator.isEmpty(input.password2)) {
+    errors.password2 = "Confirm Password field is required";
+  }
+
+  if (!validator.equals(input.password, input.password2)) {
+    errors.password2 = "Passwords must match";
+  }
+
+  return {
+    errors,
+    isvalid: isEmpty(errors)
+  };
+};
