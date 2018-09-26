f20617625dfded18aea8ff8553eab23cc1ee2bfe
Shing Wong <shing2487@gmail.com>
2018-09-26 16:22:32 -0400

    server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
