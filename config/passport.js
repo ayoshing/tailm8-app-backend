1b0d085d320069c7786a1ea384a803a21743dea8
Shing Wong <shing2487@gmail.com>
2018-09-26 12:59:18 -0400

    updated gitignore

---
+++ config/passport.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const mongoose = require('mongoose');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = mongoose.model('User');
+const keys = require('./keys');
+
+const opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+
+module.exports = passport => {
+  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    User.findById(jwt_payload.id)
+      .then(user => {
+        if(user) {
+          return done(null, user);
+        }
+        return done(null, false);
+      })
+        .catch(err => console.log(err));
+  }));
+}
