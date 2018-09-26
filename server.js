cd451c34a3e1677e96719e9b68e3ff78b11a26cc
Shing Wong <shing2487@gmail.com>
2018-09-26 16:17:53 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -37,12 +37,10 @@ app.use("/api/profile", profile, friendship);
 app.use("/api/posts", posts, likes, comments);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("tailm8-app-frontend/build"));
+  app.use(express.static("frontend/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "tailm8-app-frontend", "build", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
